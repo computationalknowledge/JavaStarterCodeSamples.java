0dfd61a880504beb06fc6d40a65eda3b136560be
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-02-06 10:16:12 -0500

    Create StarterCodeSamples1.java

---
+++ StarterCodeSamples1.java
@@ -0,0 +1,8 @@
+public class PROGRAMMINGSTARTCODE1 {
+// program code must be contained inside a METHOD:
+	public static void main(String[] args) {
+		for ( int age = 5; age<20; age=age+1  ) {
+			System.out.println(age);
+		}
+	}
+}
