417f2ede5fb5e43941dd332b61b212dc55a02458
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-02-06 13:55:45 -0500

    Update StarterCodeSamples1.java

--- StarterCodeSamples1.java
+++ StarterCodeSamples1.java
@@ -1,5 +1,14 @@
+
 public class PROGRAMMINGSTARTCODE1 {
-// program code must be contained inside a METHOD:
+/* program code must be contained inside a METHOD:
+	
+	The 4 kinds of variables we can have in any OO Language:
+		Class Instance variables: each Object of the class has its own value for that variable
+		class STATIC variables: All objects of that class share the SAME value for that variable
+		Method Local Variables
+		LOOP Local Variables
+*/
+
 	public static void main(String[] args) {
 		for ( int age = 5; age<20; age=age+1  ) {
 			System.out.println(age);
